80711387577efc2fdc7b0aa57fe4a37d483775a1
Ethan Hill <5614778+ethnhll@users.noreply.github.com>
2020-08-05 13:09:07 -0400

    make some things worse and some things better

--- katapy/fizzbuzz.py
+++ katapy/fizzbuzz.py
@@ -1,5 +1,6 @@
 def fizzbuzz(number) -> str:
     testString = "asdfkasd asdfkjaksdfklasdfkljasdlkfjla slkfjaslkdfjaklsdjf;lajsdfojawlkjfsdlka sdfklajsdflkjaslkdf asdlkj laksd jldfkaklsdjflka sdfkljasdklfjas;dlkfjas dfas dfklj"
+    testString_AGAIN = "asdfkasd asdfkjaksdfklasdfkljasdlkfjla slkfjaslkdfjaklsdjf;lajsdfojawlkjfsdlka sdfklajsdflkjaslkdf asdlkj laksd jldfkaklsdjflka sdfkljasdklfjas;dlkfjas dfas dfklj"
     result = ""
     if number % 3 == 0:
         result += 'fizz'
--- tests/test_fizzbuzz.py
+++ tests/test_fizzbuzz.py
@@ -4,3 +4,7 @@ from katapy.fizzbuzz import fizzbuzz
 def test_fizzbuzz_prime_number():
     result = fizzbuzz(7)
     assert result == '7'
+
+def test_fizzbuzz_divisible_by_three():
+    result = fizzbuzz(7*3)
+    assert result == 'fizz'
