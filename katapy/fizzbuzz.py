2063c5693a1425354e40950a3ffb213d6f8acbe6
Ethan Hill <5614778+ethnhll@users.noreply.github.com>
2020-08-05 13:59:05 -0400

    make things worse

--- katapy/fizzbuzz.py
+++ katapy/fizzbuzz.py
@@ -1,11 +1,9 @@
 def fizzbuzz(number) -> str:
-    testString = "asdfkasd asdfkjaksdfklasdfkljasdlkfjla slkfjaslkdfjaklsdjf;lajsdfojawlkjfsdlka sdfklajsdflkjaslkdf asdlkj laksd jldfkaklsdjflka sdfkljasdklfjas;dlkfjas dfas dfklj"
-    testString_AGAIN = "asdfkasd asdfkjaksdfklasdfkljasdlkfjla slkfjaslkdfjaklsdjf;lajsdfojawlkjfsdlka sdfklajsdflkjaslkdf asdlkj laksd jldfkaklsdjflka sdfkljasdklfjas;dlkfjas dfas dfklj"
     result = ""
-    if number % 3 == 0:
-        result += 'fizz'
-    if number % 5 == 0:
-        result += 'buzz'
+    # if number % 3 == 0:
+    #     result += 'fizz'
+    # if number % 5 == 0:
+    #     result += 'buzz'
     if len(result) == 0:
         result += str(number)
     return result
--- tests/test_fizzbuzz.py
+++ tests/test_fizzbuzz.py
@@ -5,6 +5,7 @@ def test_fizzbuzz_prime_number():
     result = fizzbuzz(7)
     assert result == '7'
 
-def test_fizzbuzz_divisible_by_three():
-    result = fizzbuzz(7*3)
-    assert result == 'fizz'
+
+# def test_fizzbuzz_divisible_by_three():
+#     result = fizzbuzz(7*3)
+#     assert result == 'fizz'
